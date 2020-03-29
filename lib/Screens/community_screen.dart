f044f16bc35e51983685a66dd0d74c182f5e881d
Andres Arizala <arizala13@gmail.com>
2020-03-29 16:02:19 -0500

    community icons updated

--- lib/Screens/community_screen.dart
+++ lib/Screens/community_screen.dart
@@ -34,7 +34,9 @@ class _CommunityViewScreenState extends State<CommunityViewScreen> {
                         child: Semantics(
                               label: 'Add picture',
                               child: FlatButton.icon(
-                                icon: const Icon(Icons.lightbulb_outline),
+                                icon: const Icon(Icons.lightbulb_outline,
+                                color: Colors.green
+                                ),
                                 onPressed: () {
                                   //do something
                                 }, label: Text('Volunteer'),
@@ -51,7 +53,9 @@ class _CommunityViewScreenState extends State<CommunityViewScreen> {
                         child: Semantics(
                               label: 'Add picture',
                               child: FlatButton.icon(
-                                icon: const Icon(Icons.question_answer),
+                                icon: const Icon(Icons.live_help,
+                                color: Colors.green
+                                ),
                                 onPressed: () {
                                   //do something
                                 }, label: Text('Ask for help'),
@@ -64,42 +68,81 @@ class _CommunityViewScreenState extends State<CommunityViewScreen> {
           ),
           new Container(height: 20.0,),
           new Container(
-          child: 
-          Card(
-            child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('Test post 1'),
-            ),
-          ),
-    ),
-             new Container(
-          child: 
-          Card(
-            child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('Test post 2'),
-            ),
-          ),
-    ),
-                 new Container(
-          child: 
-          Card(
-            child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('Test post 3'),
-            ),
-          ),
-    ),
-                     new Container(
-          child: 
-          Card(
-            child: ListTile(
-            leading: FlutterLogo(),
-            title: Text('Test post 3'),
-            ),
-          ),
-    )
-
+          child: Container(
+                height: 600.0,
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                        Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.lightbulb_outline,
+                              color: Colors.blue
+                              ),
+                          title: Text('Help with lawn'),
+                          subtitle: Text('I have a couple of plants..'),
+                          trailing: Icon(Icons.more_vert),
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.lightbulb_outline,
+                              color: Colors.blue
+                              ),
+                          title: Text('Free Monday 03/30 to babysit'),
+                          subtitle: Text('I am an experienced babysitter..'),
+                          trailing: Icon(Icons.more_vert),
+                          ),
+                        ),
+                      Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.live_help,
+                              color: Colors.blue
+                              ),
+                          title: Text('Need soap'),
+                          subtitle: Text('Anyone have extra hand soap?'),
+                          trailing: Icon(Icons.more_vert),
+                          ),
+                        ),
+                                              Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.live_help,
+                              color: Colors.blue
+                              ),
+                          title: Text('Gym equipment'),
+                          subtitle: Text('Does anyone have any gym equipment that.. '),
+                          trailing: Icon(Icons.more_vert),
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.lightbulb_outline,
+                              color: Colors.blue
+                              ),
+                          subtitle: Text('I have an old tv I no longer need. Does..'),
+                          trailing: Icon(Icons.more_vert),
+                          title: Text('Free TV'),
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                          leading: Icon(
+                              Icons.live_help,
+                              color: Colors.blue
+                              ),
+                          trailing: Icon(Icons.more_vert),
+                          title: Text('Plants'),
+                          subtitle: Text('Looking for plants, anyone know..'),
+                          ),
+                        )
+                  ],
+                ),
+              )
+          )
         ],
       ),),
     );
