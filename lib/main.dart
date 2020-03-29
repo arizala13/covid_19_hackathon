801a6115a276de2954a74b359ca88c20447717ef
Andres Arizala <arizala13@gmail.com>
2020-03-29 10:37:38 -0500

    news anc community together

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Screens/community_screen.dart';
-
-import 'screens/news_screen.dart';
+import 'Screens/news_screen.dart';
 
 void main() {
   runApp(MyApp());
